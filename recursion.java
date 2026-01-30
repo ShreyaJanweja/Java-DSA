c3e133eb73bef148731ab13a6affd3d4d411ea6e
ShreyaJanweja <shreyajanweja26@gmail.com>
2026-01-30 13:02:59 +0530

    print no. from 1 to n - recursion

--- recursion.java
+++ recursion.java
@@ -1,3 +1,17 @@
+//recursion by Kunal 
+
+//print no. from 1 to n
+import java.util.*;
 public class recursion {
-    
+    public static void print1toN(int n){
+    if(n==0) return;
+    print1toN(n-1);
+    System.out.print(n+" ");
+}
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int n=sc.nextInt();
+       print1toN(n);
+       sc.close();
+    }
 }
