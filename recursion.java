0db4e5365d67acc40cbd73ee78f2a3bc8623a11e
ShreyaJanweja <shreyajanweja26@gmail.com>
2026-01-30 13:05:44 +0530

    fibo. no.

--- recursion.java
+++ recursion.java
@@ -1,17 +1,36 @@
 //recursion by Kunal 
 
-//print no. from 1 to n
-import java.util.*;
-public class recursion {
-    public static void print1toN(int n){
-    if(n==0) return;
-    print1toN(n-1);
-    System.out.print(n+" ");
-}
-    public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       int n=sc.nextInt();
-       print1toN(n);
-       sc.close();
-    }
-}
+// //print no. from 1 to n
+// import java.util.*;
+// public class recursion {
+//     public static void print1toN(int n){
+//     if(n==0) return;
+//     print1toN(n-1);
+//     System.out.print(n+" ");
+// }
+//     public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n=sc.nextInt();
+//        print1toN(n);
+//        sc.close();
+//     }
+// }
+
+
+
+// //fibonaaci no. 
+// import java.util.*;
+// public class recursion {
+//     public static int fibo(int n){
+//         if(n==0) return 0;
+//         if(n==1) return 1;
+//         return fibo(n-1)+fibo(n-2);
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n=sc.nextInt();
+//         System.out.print(fibo(n));
+//         sc.close();
+//     }
+// }
