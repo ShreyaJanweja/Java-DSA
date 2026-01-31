bfcb43548ff07479309a228ecf6a8960a4b93e06
ShreyaJanweja <shreyajanweja26@gmail.com>
2026-01-31 13:05:18 +0530

    binary search using recursion

--- recursion.java
+++ recursion.java
@@ -54,3 +54,38 @@
 //         sc.close();
 //     }
 // }
+
+
+//binary search using recursion
+import java.util.*;
+public class recursion {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        int target=sc.nextInt();
+        sc.close();
+        int result = binarySearch(arr,target,0,n-1);
+        if(result==-1){
+            System.out.println("Element not found");
+        }
+        else{
+            System.out.println("Element found at idx: "+result);
+        }
+    }
+    public static int binarySearch(int arr[],int target,int start,int end){
+        if(start>end) return -1;
+        int mid=start+(end-start)/2;
+        if(arr[mid]==target) return mid;
+        else if(arr[mid]<target){
+            return binarySearch(arr, target, mid+1, end);
+        }
+        else{
+            return binarySearch(arr, target, start, mid-1);
+        }
+    }
+}
