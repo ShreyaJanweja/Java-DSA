c38aa8a845d98db5972471e8f8b05d8df396af23
ShreyaJanweja <shreyajanweja26@gmail.com>
2026-01-31 19:08:15 +0530

    factorial

--- recursion.java
+++ recursion.java
@@ -134,3 +134,20 @@
 //         System.out.print(n+" ");
 //     }
 // }
+
+
+//factorial of a no.
+import java.util.*;
+public class recursion {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(factorial(n));
+        sc.close();
+    }
+    public static int factorial(int n){
+        if(n==1) return 1;
+        return n*factorial(n-1);
+    }
+}
