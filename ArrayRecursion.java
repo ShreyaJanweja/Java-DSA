28edf5866ba0acea52079cf69a35c6e6524b8f62
ShreyaJanweja <shreyajanweja26@gmail.com>
2026-02-02 13:32:05 +0530

    linear search on multiple occurence

--- ArrayRecursion.java
+++ ArrayRecursion.java
@@ -20,7 +20,35 @@
 
 
 
-//search an element in an array using recursion
+// //search an element in an array using recursion
+// import java.util.*;
+// public class ArrayRecursion {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int arr[] = new int[n];
+//         for(int i=0; i<n; i++){
+//             arr[i]=sc.nextInt();
+//         }
+//         int target = sc.nextInt();
+//         sc.close();
+//         System.out.println(search(arr,target,0));
+//         System.out.println(findIndex(arr,target,0));
+//     }
+//     public static boolean search(int arr[],int target , int index){
+//         if(index==arr.length) return false;
+//         return arr[index]==target || search(arr, target, index+1);
+//     }
+//     public static int findIndex(int arr[],int target,int idx){
+//         if(idx==arr.length) return -1;
+//         if(arr[idx]==target) return idx;
+//         else return findIndex(arr, target, idx+1);
+//     }
+// }
+
+
+
+//linear search on multiple Occurences
 import java.util.*;
 public class ArrayRecursion {
     public static void main(String[] args) {
@@ -32,16 +60,16 @@ public class ArrayRecursion {
         }
         int target = sc.nextInt();
         sc.close();
-        System.out.println(search(arr,target,0));
-        System.out.println(findIndex(arr,target,0));
+        findAllindeces(arr, target, 0);
+        System.out.println(list);
     }
-    public static boolean search(int arr[],int target , int index){
-        if(index==arr.length) return false;
-        return arr[index]==target || search(arr, target, index+1);
+    static ArrayList<Integer> list = new ArrayList<>();
+    static void findAllindeces(int arr[], int target, int idx){
+        if(idx==arr.length) return;
+        if(arr[idx]==target){
+            list.add(idx);
+        }
+        findAllindeces(arr, target, idx+1);
     }
-    public static int findIndex(int arr[],int target,int idx){
-        if(idx==arr.length) return -1;
-        if(arr[idx]==target) return idx;
-        else return findIndex(arr, target, idx+1);
-    }
+ 
 }
