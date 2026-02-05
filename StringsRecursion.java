373ffce0bbc61aed321aa48af69b4e659d5d397a
ShreyaJanweja <shreyajanweja26@gmail.com>
2026-02-05 17:21:17 +0530

    q1 string extract 'a' another solution

--- StringsRecursion.java
+++ StringsRecursion.java
@@ -1,26 +1,54 @@
-//ques.1) Extract all a's from a string using recursion
-//input: abccad , output: bccd
+// //ques.1) Extract all a's from a string using recursion
+// //input: abccad , output: bccd
+// //method-1 ) tried myself
+// import java.util.Scanner;
+// public class StringsRecursion {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in); 
+//         String Str = sc.nextLine();
+//         sc.close();
+//         System.out.println(extractA(Str,0));
+//     }
+//     public static String ans = "";
+//     public static String extractA(String str, int idx){
+//         if (idx == str.length()) return "";
 
-import java.util.Scanner;
+//         if(str.charAt(idx)!='a') {
+//             ans=ans+str.charAt(idx);
+//             extractA(str,idx+1);
+//         }
+//         else {
+//             extractA(str,idx+1);
+//         }
+
+//         return ans;
+//     }
+// }
+
+
+
+//method-2) by kunal
+import java.util.*;
 public class StringsRecursion {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String Str = sc.nextLine();
+        String str = sc.nextLine();
         sc.close();
-        System.out.println(extractA(Str,0));
+        skip("", str);
+
     }
-    public static String ans = "";
-    public static String extractA(String str, int idx){
-        if (idx == str.length()) return "";
-
-        if(str.charAt(idx)!='a') {
-            ans=ans+str.charAt(idx);
-            extractA(str,idx+1);
+    public static void skip(String ans, String str){
+        if(str.isEmpty()){
+            System.out.println(ans);
+            return;
         }
-        else {
-            extractA(str,idx+1);
+        char ch = str.charAt(0);
+        if(ch=='a'){
+            skip(ans , str.substring(1));
         }
-
-        return ans;
+        else{
+            skip(ans+ch , str.substring(1));
+        }
     }
 }
