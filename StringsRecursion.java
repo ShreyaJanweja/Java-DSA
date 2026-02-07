8b73571c4ccf93a94062af1bcf6a1c14ac1ddc0a
ShreyaJanweja <shreyajanweja26@gmail.com>
2026-02-07 23:14:39 +0530

    revision

--- StringsRecursion.java
+++ StringsRecursion.java
@@ -55,3 +55,33 @@
 
 
 
+//empty commit 
+import java.util.*;
+public class StringsRecursion {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        sc.close();
+        skip("", str);
+
+    }
+    public static void skip(String ans, String str){
+        if(str.isEmpty()){
+            System.out.println(ans);
+            return;
+        }
+        char ch = str.charAt(0);
+        if(ch=='a'){
+            skip(ans , str.substring(1));
+        }
+        else{
+            skip(ans+ch , str.substring(1));
+        }
+    }
+}
+
+
+
+
+
