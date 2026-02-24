8dbaac3a33a1a18f1be41b040150239c1ec717ba
ShreyaJanweja <shreyajanweja26@gmail.com>
2026-02-24 18:26:48 +0530

    to check even odd using bitmasking

---
+++ bitmasking.java
@@ -0,0 +1,12 @@
+// //ques - to check even odd using bitmasking
+// import java.util.*;
+// public class bitmasking {
+
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         sc.close();
+//         if((n & 1) == 1) System.out.println("odd");
+//         else System.out.println("even");
+//     }
+// }
