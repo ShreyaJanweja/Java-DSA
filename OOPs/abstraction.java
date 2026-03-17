70678d38eba980992fb4961eda146916b977487b
ShreyaJanweja <shreyajanweja26@gmail.com>
2026-03-17 12:28:17 +0530

    Interface in abstraction

--- OOPs/abstraction.java
+++ OOPs/abstraction.java
@@ -1,24 +1,48 @@
-//abstract class
-package OOPs;
-abstract class animal{
-    public abstract void sound();
-}
-class Dog extends animal{
-    public void sound(){
-        System.out.println("Dog is barking");
-    }
-}
-class Lion extends animal{
-    public void sound(){
-        System.out.println("Lion is roar");
-    }
-}
+// //abstract class
+// package OOPs;
+// abstract class animal{
+//     public abstract void sound();
+// }
+// class Dog extends animal{
+//     public void sound(){
+//         System.out.println("Dog is barking");
+//     }
+// }
+// class Lion extends animal{
+//     public void sound(){
+//         System.out.println("Lion is roar");
+//     }
+// }
 
-public class abstraction {
-    public static void main(String[] args) {
-        Dog d = new Dog();
-        Lion l = new Lion();
-        d.sound();
-        l.sound();
+// public class abstraction {
+//     public static void main(String[] args) {
+//         Dog d = new Dog();
+//         Lion l = new Lion();
+//         d.sound();
+//         l.sound();
+//     }
+// }
+
+
+
+//interface 
+package OOPs;
+interface Animal
+{
+    void sound();    //public + abstract
+}
+class Dog implements Animal
+{
+    public void sound()
+    {
+        System.out.println("Dog barks");
+    }
+}
+public class abstraction
+{
+    public static void main(String[] args)
+    {
+        Dog d = new Dog();   // object create
+        d.sound();           // method call
     }
 }
