73d07c0a291de27c484e21386ccaae145d1161e9
ShreyaJanweja <shreyajanweja26@gmail.com>
2026-03-23 12:30:17 +0530

    default constructor

--- OOPs/classObject.java
+++ OOPs/classObject.java
@@ -1,20 +1,43 @@
+// this file has class & object + constructor + access modifiers question 
 package OOPs;
 
-//basic class & object code
+// //basic class & object code
+// class Student{
+//       String name;
+//     int age;
+
+//     void display(){
+//         System.out.println("Shreya");
+//         System.out.println(20);
+//     }
+// }
+
+// public class classObject {
+//     public static void main(String[] args) {
+//         Student obj = new Student();
+//         obj.display();
+//     }
+// }
+
+
+//default constructor
 class Student{
-      String name;
+    String name;
     int age;
 
+    Student(){
+        name="Shreya";
+        age=20;
+    }
+
     void display(){
-        System.out.println("Shreya");
-        System.out.println(20);
+        System.out.println(name);
+        System.out.println(age);
     }
 }
-
 public class classObject {
     public static void main(String[] args) {
-        Student obj = new Student();
-        obj.display();
+        Student s1 = new Student();
+        s1.display();
     }
 }
-
