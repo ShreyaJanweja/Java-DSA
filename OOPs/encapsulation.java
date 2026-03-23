d83132808e7d8944669c89fffce76b12988183b2
ShreyaJanweja <shreyajanweja26@gmail.com>
2026-03-23 13:29:46 +0530

    Encapsulation

--- OOPs/encapsulation.java
+++ OOPs/encapsulation.java
@@ -1,5 +1,30 @@
+//encapsulation - data hiding (for unautherized users)
 package OOPs;
+class BankAccount{
+    private double balance;
 
+    void deposit(double amount){
+        balance += amount;
+    }
+
+    void withdraw(double amount){
+        if(amount <= balance){
+            balance -= amount;
+        }
+        else{
+            System.out.println("Insufficient balance");
+        }
+    }
+
+    double getBalance(){
+        return balance;
+    }
+}
 public class encapsulation {
-    
+    public static void main(String[] args) {
+        BankAccount obj = new BankAccount();
+        obj.deposit(1000);
+        obj.withdraw(400);
+        System.out.println(obj.getBalance());
+    }
 }
