ecba9aafa08bd0d2428b1cfc762bca46e4bb4200
ShreyaJanweja <shreyajanweja26@gmail.com>
2026-03-23 12:43:12 +0530

    Constructor overloading

--- OOPs/classObject.java
+++ OOPs/classObject.java
@@ -69,10 +69,44 @@ package OOPs;
 
 
 
-//copy constructor
+// //copy constructor
+// class Student{
+//     String name;
+//     int age;
+
+//     Student(String name, int age){
+//        this.name = name;
+//        this.age = age;
+//     }
+
+//     Student(Student s){
+//         this.name = s.name;
+//         this.age = s.age;
+//     }
+
+//     void display(){
+//         System.out.println(name);
+//         System.out.println(age);
+//     }
+// }
+// public class classObject {
+//     public static void main(String[] args) {
+//         Student s1 = new Student("Shreya",20);
+//         s1.display();
+//     }
+// }
+
+
+
+//constructor overloading
 class Student{
     String name;
     int age;
+
+    Student(){
+        name="Shreya";
+        age=20;
+    }
 
     Student(String name, int age){
        this.name = name;
@@ -91,7 +125,11 @@ class Student{
 }
 public class classObject {
     public static void main(String[] args) {
-        Student s1 = new Student("Shreya",20);
+        Student s1 = new Student();
+        Student s2 = new Student("Srishti",19);
+        Student s3 = new Student(s1);
         s1.display();
+        s2.display();
+        s3.display();
     }
 }
