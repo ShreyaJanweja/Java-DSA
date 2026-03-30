c07fe368d65701eeb011c3a50d1125d67ff2405b
ShreyaJanweja <shreyajanweja26@gmail.com>
2026-03-30 18:15:31 +0530

    Null pounter exception

--- exceptionHandling/Exceptions.java
+++ exceptionHandling/Exceptions.java
@@ -1,16 +1,33 @@
 package exceptionHandling;
-//arithematic exception - when we try to divide a number by zero
-public class Exceptions {
-    public static void main(String[] args) {
-        int a=10,b=0,c;
-        // c=a/b; // this will throw an exception because we cannot divide by zero
-        // System.out.println(c);
-        try{
-            c=a/b;
-            System.out.println(c);
-        }
-        catch(ArithmeticException e){
-            System.out.println("Cannot divide by zero");
-        }
-    }
-}
+
+// //arithematic exception - when we try to divide a number by zero
+// public class Exceptions {
+//     public static void main(String[] args) {
+//         int a=10,b=0,c;
+//         // c=a/b; // this will throw an exception because we cannot divide by zero
+//         // System.out.println(c);
+//         try{
+//             c=a/b;
+//             System.out.println(c);
+//         }
+//         catch(ArithmeticException e){
+//             System.out.println("Cannot divide by zero");
+//         }
+//     }
+// }
+
+
+// // null pointer exception - when we try to access a null object
+// public class Exceptions {
+
+//     public static void main(String[] args) {
+//         String str = null;
+//         // System.out.println(str.length()); // this will throw an exception because we cannot access a null object
+//         try{
+//             System.out.println(str.toUpperCase());
+//         }
+//         catch(NullPointerException e){
+//             System.out.println("null pointer can be casted");
+//         }
+//     }
+// }
