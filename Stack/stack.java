8bb5e924611629d913a3c095486428e19b1d59e9
ShreyaJanweja <shreyajanweja26@gmail.com>
2026-04-09 11:07:21 +0530

    insert at bottom of the stack

--- Stack/stack.java
+++ Stack/stack.java
@@ -24,7 +24,34 @@ package Stack;
 
 
 
-//ques1.) copy contents of one stack to another stack in same order
+// //ques1.) copy contents of one stack to another stack in same order
+// import java.util.*;
+// class Main{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int n=sc.nextInt();
+//         Stack<Integer> st = new Stack<>();
+//         for(int i=0; i<n; i++){
+//             int x=sc.nextInt();
+//             st.push(x);
+//         }
+//         sc.close();
+//         System.out.println("Original Stack : "+st);
+//         Stack<Integer> revSt = new Stack<>();
+//         while(st.size()>0){
+//             revSt.push(st.pop());
+//         }
+//         Stack<Integer> copySt = new Stack<>();
+//         while(revSt.size()>0){
+//             copySt.push(revSt.pop());
+//         }
+//         System.out.println("copied stack : "+copySt);
+//     }
+// }
+
+
+
+//2.) insert at bottom of the stack 
 import java.util.*;
 class Main{
     public static void main(String args[]){
@@ -32,19 +59,20 @@ class Main{
         int n=sc.nextInt();
         Stack<Integer> st = new Stack<>();
         for(int i=0; i<n; i++){
-            int x=sc.nextInt();
+            int x = sc.nextInt();
             st.push(x);
         }
+        System.out.println("original stack : "+st);
+        int num = sc.nextInt();
         sc.close();
-        System.out.println("Original Stack : "+st);
-        Stack<Integer> revSt = new Stack<>();
+        Stack<Integer> helperSt = new Stack<>();
         while(st.size()>0){
-            revSt.push(st.pop());
+            helperSt.push(st.pop());
         }
-        Stack<Integer> copySt = new Stack<>();
-        while(revSt.size()>0){
-            copySt.push(revSt.pop());
+        st.push(num);
+        while(helperSt.size()>0){
+            st.push(helperSt.pop());
         }
-        System.out.println("copied stack : "+copySt);
+        System.out.println("new stack : "+st);
     }
 }
