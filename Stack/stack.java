8421de3e09fc10b86876883193cc7ffedf2a803a
ShreyaJanweja <shreyajanweja26@gmail.com>
2026-04-09 11:00:21 +0530

    copy contents of one stack to another stack in same order

--- Stack/stack.java
+++ Stack/stack.java
@@ -1,22 +1,50 @@
 package Stack;
+// import java.util.*;
+// class Codechef
+// {
+// 	public static void main (String[] args) throws java.lang.Exception
+// 	{
+// 	    Scanner sc = new Scanner(System.in);
+// 	    int n=sc.nextInt();
+// 	    Stack<Integer> st = new Stack<>();
+// 	    for(int i=0; i<n; i++){
+// 	        int x = sc.nextInt();
+// 	        st.push(x);
+// 	    }
+//         sc.close();
+// 	    System.out.println(st);
+// 	    System.out.println(st.push(90));
+// 	    System.out.println(st.peek());
+// 	    System.out.println(st.pop());
+// 	    System.out.println(st.size());
+// 	    System.out.println(st.isEmpty());
+// 	}
+// }
+
+
+
+
+//ques1.) copy contents of one stack to another stack in same order
 import java.util.*;
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-	    Scanner sc = new Scanner(System.in);
-	    int n=sc.nextInt();
-	    Stack<Integer> st = new Stack<>();
-	    for(int i=0; i<n; i++){
-	        int x = sc.nextInt();
-	        st.push(x);
-	    }
+class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        Stack<Integer> st = new Stack<>();
+        for(int i=0; i<n; i++){
+            int x=sc.nextInt();
+            st.push(x);
+        }
         sc.close();
-	    System.out.println(st);
-	    System.out.println(st.push(90));
-	    System.out.println(st.peek());
-	    System.out.println(st.pop());
-	    System.out.println(st.size());
-	    System.out.println(st.isEmpty());
-	}
+        System.out.println("Original Stack : "+st);
+        Stack<Integer> revSt = new Stack<>();
+        while(st.size()>0){
+            revSt.push(st.pop());
+        }
+        Stack<Integer> copySt = new Stack<>();
+        while(revSt.size()>0){
+            copySt.push(revSt.pop());
+        }
+        System.out.println("copied stack : "+copySt);
+    }
 }
