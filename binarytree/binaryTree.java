19ef89cacee24c8a9b96027c6ee69d96260214e2
ShreyaJanweja <shreyajanweja26@gmail.com>
2026-05-04 13:54:00 +0530

    Post order traversal

--- binarytree/binaryTree.java
+++ binarytree/binaryTree.java
@@ -87,3 +87,45 @@
 //         inorder(root);
 //     }
 // }
+
+
+
+
+// //postorder = left, right, root
+// import java.util.*;
+
+// class Node{
+//     int data;
+//     Node left;
+//     Node right;
+    
+//     Node(int data){
+//         this.data = data;
+//         this.left = null;
+//         this.right = null;
+//     }
+// }
+// class BinaryTree{
+//     static int idx = -1;
+//     public static Node buildTree(int[] nodes){
+//         idx++;
+//         if(nodes[idx]==-1) return null;
+//         Node newNode = new Node(nodes[idx]);
+//         newNode.left = buildTree(nodes);
+//         newNode.right = buildTree(nodes);
+//         return newNode;
+//     }
+//     public static void postorder(Node root){
+//         if(root == null){
+//             return;
+//         }
+//         postorder(root.left);
+//         System.out.print(root.data+" ");
+//         postorder(root.right);
+//     }
+//     public static void main(String args[]){
+//         int[] nodes = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+//         Node root = buildTree(nodes);
+//         postorder(root);
+//     }
+// }
